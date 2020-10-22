cda010ce4f3a5627722b3be59cf1cc5dc3bc00f1
adbast <adbast@kitron.com>
2020-10-22 23:01:37 +0200

    podstawowa funkcjonalnosc

--- PrintBarcodeZebra/Form1.Designer.cs
+++ PrintBarcodeZebra/Form1.Designer.cs
@@ -28,13 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.snBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.tboxPrinter = new System.Windows.Forms.TextBox();
+            this.lblPrinter = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(84)))), ((int)(((byte)(0)))));
+            this.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPrint.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnPrint.Location = new System.Drawing.Point(297, 120);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(109, 34);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "DRUKUJ";
+            this.btnPrint.UseVisualStyleBackColor = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // snBox
+            // 
+            this.snBox.Location = new System.Drawing.Point(53, 68);
+            this.snBox.Name = "snBox";
+            this.snBox.Size = new System.Drawing.Size(353, 20);
+            this.snBox.TabIndex = 1;
+            this.snBox.TextChanged += new System.EventHandler(this.snBox_TextChanged);
+            this.snBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.snBox_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Niagara Engraved", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zeskanuj numer seryjny produktu";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panel1.Controls.Add(this.lblPrinter);
+            this.panel1.Controls.Add(this.tboxPrinter);
+            this.panel1.Controls.Add(this.btnSettings);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 201);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(468, 30);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Location = new System.Drawing.Point(393, 0);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(75, 30);
+            this.btnSettings.TabIndex = 0;
+            this.btnSettings.Text = "Ustawienia";
+            this.btnSettings.UseVisualStyleBackColor = false;
+            this.btnSettings.Visible = false;
+            this.btnSettings.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelMain
+            // 
+            this.panelMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(126)))), ((int)(((byte)(34)))));
+            this.panelMain.Controls.Add(this.lblClose);
+            this.panelMain.Controls.Add(this.label1);
+            this.panelMain.Controls.Add(this.snBox);
+            this.panelMain.Controls.Add(this.btnPrint);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(0, 0);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(468, 201);
+            this.panelMain.TabIndex = 4;
+            this.panelMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelMain_MouseDown);
+            // 
+            // lblClose
+            // 
+            this.lblClose.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblClose.Location = new System.Drawing.Point(452, 0);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(16, 17);
+            this.lblClose.TabIndex = 3;
+            this.lblClose.Text = "X";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            // 
+            // tboxPrinter
+            // 
+            this.tboxPrinter.Location = new System.Drawing.Point(87, 7);
+            this.tboxPrinter.Name = "tboxPrinter";
+            this.tboxPrinter.Size = new System.Drawing.Size(178, 20);
+            this.tboxPrinter.TabIndex = 1;
+            // 
+            // lblPrinter
+            // 
+            this.lblPrinter.AutoSize = true;
+            this.lblPrinter.Location = new System.Drawing.Point(3, 10);
+            this.lblPrinter.Name = "lblPrinter";
+            this.lblPrinter.Size = new System.Drawing.Size(81, 13);
+            this.lblPrinter.TabIndex = 2;
+            this.lblPrinter.Text = "Nazwa drukarki";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.ClientSize = new System.Drawing.Size(468, 231);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Drukowanie etykiet";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.TextBox snBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.Label lblPrinter;
+        private System.Windows.Forms.TextBox tboxPrinter;
     }
 }
 
--- PrintBarcodeZebra/Form1.cs
+++ PrintBarcodeZebra/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,119 @@ namespace PrintBarcodeZebra
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if (snBox.Text.Length > 15)
+            {
+                string dataToSend = GetZplData(snBox.Text);
+                // Allow the user to select a printer.
+                if (tboxPrinter.Text == "")
+                {
+                    PrintDialog pd = new PrintDialog();
+                    pd.PrinterSettings = new PrinterSettings();
+                    if (DialogResult.OK == pd.ShowDialog(this))
+                    {
+                        tboxPrinter.Text = pd.PrinterSettings.PrinterName;
+                    }
+                }
+                RawPrinterHelper.SendStringToPrinter(tboxPrinter.Text, dataToSend);
+                snBox.Text = "";
+                snBox.Focus();
+                //PrintDialog pd = new PrintDialog();
+                //pd.PrinterSettings = new PrinterSettings();
+                //if (DialogResult.OK == pd.ShowDialog(this))
+                //{
+                //    // Send a printer-specific to the printer.
+                //    RawPrinterHelper.SendStringToPrinter(pd.PrinterSettings.PrinterName, dataToSend);
+                    
+                //    //MessageBox.Show("Data sent to printer.");
+                //    snBox.Clear();
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Data not sent to printer.");
+                //}
+            }
+            
+        }
+
+        private string GetZplData(string serialNumber)
+        {
+            StringBuilder zpl = new StringBuilder();
+            zpl.Append(@"CT~~CD,~CC^~CT~
+                        ^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR2,2~SD30^JUS^LRN^CI0^XZ
+                        ^XA
+                        ^MMT
+                        ^PW243
+                        ^LL0070
+                        ^LS0
+                        ^FT169,58^BQN,2,2
+                        ^FH\^FDMA,<PARAM>^FS
+                        ^FT165,26^A0I,8,14^FH\^FD<PARAM1>^FS
+                        ^FT165,16^A0I,8,14^FH\^FD<PARAM2>^FS
+                        ^PQ1,0,1,Y^XZ
+                        ");
+
+            string param1 = serialNumber.Substring(0, 10);
+            string param2 = serialNumber.Substring(10);
+
+            zpl.Replace("<PARAM>", serialNumber);
+            zpl.Replace("<PARAM1>", param1);
+            zpl.Replace("<PARAM2>", param2);
+
+            return zpl.ToString();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HideControls();
+            lblClose.Show();
+        }
+        
+        private void HideControls()
+        {
+            foreach (Control ctrl in panelMain.Controls)
+            {
+                ctrl.Hide();
+            }
+        }
+
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void snBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void snBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnPrint.PerformClick();
+                
+            }
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void panelMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
